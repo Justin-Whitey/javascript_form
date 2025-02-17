a96a33c6198ae8fa37e54887f4f7b344bf609254
COMLAB <jbcatulong23-0340@cca.edu.ph>
2025-02-17 17:51:41 +0800

    Javaform
    form na java

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Javascript Form</title>
+	</head>
+	
+	<body style="background-color: black; color: #FFF;">
+		<h1> tara shabu </h1>
+		<table id="student_table">
+		</table>
+		
+		<script>
+			const studentTable = document.getElementById("student_table");
+			
+			console.log(studentTable);
+		</script>
+	</body>
+</html>
