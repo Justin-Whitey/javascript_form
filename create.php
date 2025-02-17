8cb4fba86fe8656b28cd559d8cc99c1b41511051
COMLAB <jbcatulong23-0340@cca.edu.ph>
2025-02-17 19:43:16 +0800

    push

--- create.php
+++ create.php
@@ -1,7 +1,10 @@
 <?php
 	echo "Hello world";	
 	var_dump($_POST);
+	$lastname = $_POST['last_name'];
+	$firstname = $_POST['first_name'];
 ?>
+<br>
 <br>
 <br>
 <br>
@@ -12,11 +15,24 @@ $password = "";
 $database = "lorraine";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$sql = "INSERT INTO students (id, last_name, first_name)
+VALUES ('', '$lastname', '$firstname')";
+
+if ($conn->query($sql) === TRUE) {
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+  
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 ?>
